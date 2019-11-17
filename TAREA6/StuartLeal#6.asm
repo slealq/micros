b74b5c334aa2e184dc110b35c14c3af9d4e84172
Stuart Leal <stuart.leal23@gmail.com>
2019-11-16 18:15:07 -0600

    Add Calculo and SC1 logic
    - Calculate level in meters
    - Calculate volume in meters^3
    - Sends messages using an EOL, and TC interupt via SC1

--- TAREA6/StuartLeal#6.asm
+++ TAREA6/StuartLeal#6.asm
@@ -10,16 +10,29 @@
 ;; ==================== DECLARACION DE ESTRUCTURAS DE DATOS ==================
 ;; ===========================================================================
 
-                        org $1000
+MAX_TANQUE:             EQU 30
+AREA100:                EQU 1963
+EOL:                    EQU 0
 
-NIVEL_PROM:             ds 1          
+                        org $1010
+NIVEL_PROM:             ds 2
+NIVEL:                  ds 1
+VOLUMEN                 ds 2     
+
+POS:                    ds 2
+
+TEST_MSG:               fcc 'Este es un mensaje de prueba'
+                        db EOL
 
 ;; ===========================================================================
 ;; ==================== DECLARACION DE INTERRUPCIONES ========================
 ;; ===========================================================================
 
                         org $3E52
-                        dw ATD0_ISR                 
+                        dw ATD0_ISR        
+
+                        org $3E54
+                        dw SCI1_ISR         
 
 ;; ===========================================================================
 ;; ==================== CODIFICACION HARDWARE ================================
@@ -41,6 +54,10 @@ MN_After_10us           movb #$30 ATD0CTL3
                         movb #$10 ATD0CTL4
                         movb #$87 ATD0CTL5
 
+                        movw #39 SC1BDH
+                        movb #$12 SC1CR1
+                        movb #$08 SC1CR2
+
                         cli                        
 
 ;; ===========================================================================
@@ -51,9 +68,12 @@ MN_After_10us           movb #$30 ATD0CTL3
 ;; ===========================================================================
 ;; ==================== PROGRAMA PRINCIPAL ===================================
 ;; ===========================================================================
-                                 
 
-MN_fin                  bra *
+                        movw #TEST_MSG POS
+                        bset SC1CR2,$40
+
+MN_fin                  jsr CALCULO
+                        bra MN_fin                        
 
 ;; ==================== Subrutina ATD0_ISR ===================================
 
@@ -72,6 +92,48 @@ ATD0_ISR                ldd ADR00H
 
 ;; ==================== Subrutina CALCULO ====================================
 
-CALCULO                 
+CALCULO                 ldd NIVEL_PROM
+                        ldy #MAX_TANQUE
+                        emul
+                        ldx #$3ff
+                        ediv
 
+                        cpd #500
+                        blo CALCULO_sv_nivel
+
+                        iny
+
+CALCULO_sv_nivel        tfr y,a
+                        staa NIVEL
+                        ldd #AREA100
+                        emul
+                        ldx #100
+                        idiv 
+
+                        cpd #50
+                        blo CALCULO_sv_volumen
+
+                        inx
+
+CALCULO_sv_volumen      stx VOLUMEN                       
+                        
+                        rts
+
+;; ==================== Subrutina SCI1_ISR ====================================
+
+SCI1_ISR                ldx POS
+                        ldaa 1,x+
+                        stx POS
+
+                        cmpa EOL
+                        beq SCI1_disable
+
+                        ldab SC1SR1
+                        staa SC1DRL
+
+                        bra SC1_return
+
+SCI1_disable            bclr SC1CR2,$40
+
+SC1_return              rti                        
 
